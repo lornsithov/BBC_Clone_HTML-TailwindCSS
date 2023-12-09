7872236ad1ca604578f5141849cfad135a2c85eb
lornsithov <lornsithov@gmai.com>
2023-12-08 21:13:34 -0500

    commit fr

---
+++ js/config.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      backgroundImage: {
+          future: "url('../img/future01.jpg')",
+          future02: "url('../img/future07c.jpg')",
+          travel: "url('../img/future.jpg')",
+      }
+    }
+  }
+}
